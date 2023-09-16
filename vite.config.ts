05f65b925c96b901a7cbb6148e5c40d4fc5c01d1
breeg554 <dvdk98@gmail.com>
2023-09-16 22:58:13 +0200

    Add build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['cjs', 'es'],
+      name: 'index',
+      fileName: 'index',
+    },
+  },
+  plugins: [react(), dts()],
+});
